07f5a320ce4fd03ea4476755c646b67762b026ad
Aristotle-Mont <real-hollow484@hotmail.com>
2016-12-18 03:36:30 -0500

    lkl

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Danilo Montalvo  on 12/17/16.
+//  Copyright © 2016 MySelf. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
