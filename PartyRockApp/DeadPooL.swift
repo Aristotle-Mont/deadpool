6c236286314baaf356fa49d22a7600cc0f7eeae1
Aristotle-Mont <real-hollow484@hotmail.com>
2016-12-20 04:44:10 -0500

    go

---
+++ PartyRockApp/DeadPooL.swift
@@ -0,0 +1,9 @@
+//
+//  DeadPooL.swift
+//  PartyRockApp
+//
+//  Created by Danilo Montalvo  on 12/19/16.
+//  Copyright © 2016 MySelf. All rights reserved.
+//
+
+import Foundation
---
+++ PartyRockApp/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRockApp
+//
+//  Created by Danilo Montalvo  on 12/19/16.
+//  Copyright © 2016 MySelf. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
